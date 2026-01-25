b6bf17b4bff7c3cdf8d21433c95a7aa3f13eff5d
Tmotomaster <timo.anishchuk@gmail.com>
2026-01-25 13:11:37 +0200

    Set euclidean distance as default; added "flatter"

--- distance.cpp
+++ distance.cpp
@@ -54,8 +54,9 @@ struct Distance {
 queue<Distance*> to_check;
 
 float calc_distance(int y1, int x1, int y2, int x2) {
-  return (float)(abs(y2 - y1) + abs(x2 - x1));
-  // return sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+  // return (float)(abs(y2 - y1) + abs(x2 - x1)); // Manhattan
+  return sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)); // Euclidean
+  // return sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)) + abs(y2 - y1); // Flatter
 }
 
 int main() {
