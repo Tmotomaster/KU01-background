e1972508be7ea041401ea6fb7b59dd00d28d0daf
Tmotomaster <timo.anishchuk@gmail.com>
2026-02-04 21:57:23 +0200

    Added explosionlines.cpp

---
+++ explosionlines.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+
+typedef unsigned int uint;
+
+using namespace std;
+
+const uint width = 3840;
+const uint height = 2160;
+
+const uint particle_count = 10000;
+const uint plength_min = 50;
+const uint plength_var = 250;
+
+void createppm(const string path, uint8_t* data) {
+  ofstream f(path, ios::binary);
+  f << "P6\n" << width << ' ' << height << "\n255\n";
+  for (uint i = 0; i < width * height * 3; i++) {
+    f << data[i];
+  }
+  f.close();
+  cout << "Generated " << path << endl;
+}
+
+int main() {
+  cout << "ueaaaa" << endl;
+  srand(time(0));
+
+  uint8_t* original = new uint8_t[width * height * 3];
+
+  int centerX = width >> 1;
+  int centerY = height >> 1;
+
+
+  for (uint t = 0; t < particle_count; t++) {
+    int x = rand() % width;
+    int y = rand() % height;
+    int length = plength_min + rand() % plength_var;
+
+    int fromcenterX = x - centerX;
+    int fromcenterY = y - centerY;
+
+    float fromc_length = sqrt(fromcenterX*fromcenterX + fromcenterY*fromcenterY);
+
+    float normX = (float)fromcenterX / fromc_length;
+    float normY = (float)fromcenterY / fromc_length;
+
+    if (abs(normX) > abs(normY)) {
+      for (int i = 0; i < abs(normX * length); i++) {
+        int actualX = x + (int)(normX * i + .5f);
+        int actualY = y + (int)(i / abs(normX * length) * normY * length + .5f);
+
+        if (actualX < 0 || actualX >= width || actualY < 0 || actualY >= height) break;
+
+        original[3 * (width * actualY + actualX)    ] = 255;
+        original[3 * (width * actualY + actualX) + 1] = 255;
+        original[3 * (width * actualY + actualX) + 2] = 255;
+      }
+    } else {
+      for (int i = 0; i < abs(normY * length); i++) {
+        int actualX = x + (int)(i / abs(normY * length) * normX * length + .5f);
+        int actualY = y + (int)(normY * i + .5f);
+
+        if (actualX < 0 || actualX >= width || actualY < 0 || actualY >= height) break;
+
+        original[3 * (width * actualY + actualX)    ] = 255;
+        original[3 * (width * actualY + actualX) + 1] = 255;
+        original[3 * (width * actualY + actualX) + 2] = 255;
+      }
+    }
+  }
+
+  createppm("sustest.ppm", original);
+
+
+  return 0;
+}
